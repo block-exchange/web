30857c5928159fd1d3ae05abda45c349b2b9bc2e
aditya singh <adityasingh27@hotmail.com>
2020-09-24 12:25:57 +0530

    Updates

---
+++ precache-manifest.8888d271558626dd1125da0fa27f03f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ae08a32f0425b5308e577dfc41ff236",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "28d7b0286d6b76b993aa",
+    "url": "/web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "951072c986f469110060",
+    "url": "/web/static/js/2.a011838f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web/static/js/2.a011838f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d7b0286d6b76b993aa",
+    "url": "/web/static/js/main.96a89652.chunk.js"
+  },
+  {
+    "revision": "932666a3d42c492ed9a6",
+    "url": "/web/static/js/runtime-main.ac4df23f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  }
+]);
