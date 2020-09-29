26d9a0cff807bf6a419b2276a86379e029468563
aditya singh <adityasingh27@hotmail.com>
2020-09-29 12:49:34 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class App extends Component {
     return (
       <div className="App">
         <MyAppBar/>
+        <div className="App-header"/>
         <SearchBarComponent/>
         <RepoList/>
       </div>
