9d4dadee648a7209f4d25b00e044bcffd5713e2c
aditya singh <adityasingh27@hotmail.com>
2020-09-29 14:20:31 +0530

    graphql

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import ApolloClient from 'apollo-boost';
+import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from 'apollo-link-http';
+
 import axios from 'axios';
 
 import './App.css';
@@ -9,23 +12,37 @@ import AppReviewCard from './components/CardComponent.js'
 import SearchBarComponent from './components/SearchComponent.js'
 import RepoList from './components/RepoList.js'
 import MyAppBar from './components/navbar.js'
+import RepoImage from './components/query.js'
 
 
 
-const client = new ApolloClient({
-      uri : "https://developer.github.com/v4/explorer/"
+
+const client = new ApolloClient({ 
+  link: new createHttpLink(
+      {
+        uri : "https://developer.github.com/v4/explorer/",
+      }
+  ),
+  cache: new InMemoryCache(),
+  fetchOptions: {
+    credentials: 'include'
+
+  },
 });
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MyAppBar/>
-        <div className="App-header"/>
-        <SearchBarComponent/>
-        <RepoList/>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <MyAppBar/>
+          <div className="App-header"/>
+          <SearchBarComponent/>
+          <RepoList/>
+          <RepoImage/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
