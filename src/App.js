73f13d4d062be970f28e92e5df4ec479626299d5
aditya singh <adityasingh27@hotmail.com>
2021-03-18 17:42:16 +0530

    react router

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ import SearchBarComponent from './components/SearchComponent.js'
 import RepoList from './components/RepoList.js'
 import MyAppBar from './components/navbar.js'
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-        <div className="App">
-          <MyAppBar/>
-          <div className="App-header">
-            <img src={process.env.PUBLIC_URL + "/images/1.png"}/></div>
-          <SearchBarComponent/>
-          <RepoList/>
-          <div className="App-header"/>
-        </div>
+	    <Router>
+		<Switch>
+			<Route path="/" exact component= {Home} />
+			<Route path="/card" component= {AppReviewCard} />
+		</Switch>
+       
+		</Router>
     );
   }
 }
+
+const Home = () => (
+
+	<div className="App">     
+	<MyAppBar/>
+	<div className="App-header">
+	<img src={process.env.PUBLIC_URL + "/images/1.png"}/></div>
+	<SearchBarComponent/>
+	<RepoList/>
+	<div className="App-header"/>
+    </div>
+
+);
 
 export default App;
